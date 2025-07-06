9eae33a181411ad88e45b9d5c7a92617919f2ec7
unknown <lawzyaxy@gmail.com>
2025-07-06 12:42:52 +0000

    Finished Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        // Ask for grade percentage
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        console.WriteLine($"Your grade is: {percent}%");
+
+        // Determine letter grade based on percentage
+        // A: 90-100, B: 80-89, C: 70-79, D: 60-69, F: 0-59
+        // Print the letter grade and a message based on the grade
+
+        string letter = "";
+    
+        if (percent >= 90)
+        {
+            letter = "A";
+            Console.WriteLine(letter);
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+            Console.WriteLine(letter);
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+            Console.WriteLine(letter);
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+            Console.WriteLine(letter);
+        }
+        else
+        {
+            letter = "F";
+            Console.WriteLine(letter);
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            // If the grade is 70 or above, print a message congratulating the user
+            Console.WriteLine("Congratulations!");
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            // If the grade is below 70, print a message encouraging the user 
+            Console.WriteLine("Better luck next time!");
+            Console.WriteLine("You can do it!");
+            Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        }
+     }
 }
+
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
+         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Ask for the magic number
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        
+        bool hasGuessedCorrectly = false;
+        
+        // It will Continue to keep asking for guesses until the user guesses correctly
+        while (!hasGuessedCorrectly)
+        {
+            // Ask for the user's guess
+            Console.Write("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
+            
+            // Compare the guess with the magic number
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                hasGuessedCorrectly = true;
+            }
+            
+            // Add a blank line for better readability
+            Console.WriteLine();
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        }
     }
 }
