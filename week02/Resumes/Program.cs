58fd388969ca666b12d978f03763a581674bf46d
Lawzyaxy1 <142256529+Lawzyaxy1@users.noreply.github.com>
2025-07-12 19:58:24 +0000

    Update Program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+    // Create Job 
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+
+        // Create a Resume 
+        Resume myResume = new Resume("Allison Rose");
+
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display the resume
+        myResume.DisplayResume();
     }
 }
